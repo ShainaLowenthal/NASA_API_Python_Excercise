677b9ceaabdf09cbaa15853b101343841feda0ac
ShainaLowenthal <37282752+ShainaLowenthal@users.noreply.github.com>
2020-02-18 23:05:38 -0500

    combined downloading jpgs and updating metadata csv

--- NASA_API.py
+++ NASA_API.py
@@ -9,11 +9,24 @@ response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosit
 
 print(response.status_code)
 
-for item in response.json()['photos']:
-    source = item['img_src']
-    name = source[source.rfind("/"):]
-    response = requests.get(source)
-    with open("/Users/shainalowenthal/Desktop/NASA"+name, "wb") as image:
-        image.write(response.content)
+fieldnames = list(response.json()['photos'][0].keys())
+fieldnames=fieldnames+['camera_id',"camera_name","camera_rover_id","camera_full_name"]
+with open('/Users/shainalowenthal/Desktop/meta.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for item in response.json()['photos']:
+        item['camera_id']=item['camera']['id']
+        item['camera_name']=item['camera']['name']
+        item['camera_rover_id']=item['camera']['rover_id']
+        item['camera_full_name']=item['camera']['full_name']
+        writer.writerow(item)
+        source = item['img_src']
+        name = source[source.rfind("/"):]
+        response = requests.get(source)
+        with open("/Users/shainalowenthal/Desktop/NASA"+name, "wb") as image:
+            image.write(response.content)
+    
+       
+    
 print("done")
 
